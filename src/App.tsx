b0b0e824414c24c4f3076b79010402a8a22b0da1
choiyounghwan <egoing@gmail.com>
2022-11-10 16:59:52 +0900

    ver1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/'></Route>
+        <Route path='/login'></Route>
+        <Route path='/main'></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
