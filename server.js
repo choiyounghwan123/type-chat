cda57939e546d4bae0407b06305a0eb61fa2abf1
choiyounghwan <egoing@gmail.com>
2022-11-10 18:39:11 +0900

    ver1.4

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const app = require('express')();
 const http  = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http,{
+    cors:{
+        origin:"*"
+    }
+});
 
-io.on('connetion', socket =>{
+io.on('connection', socket =>{
     console.log(' a user connected');
 })
 
