7fed4dbbc3d2f68775e5a6eb9ded1e93c0c6cc24
chigomuh <tngusrldnjs1@gmail.com>
2022-11-11 22:28:49 +0900

    Revert "1.4.7"
    This reverts commit 7d5788097ce8e9e38076871c8bc49d9d532ff1af.

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -1,12 +1,9 @@
-import { AppBar, Drawer } from "@mui/material";
 import Menu from "../component/Menu";
-import BasicExample from "../component/Navbar";
-import Navbar from "../component/Navbar";
 
 const Main = () =>{
     return(
         <> 
-        <BasicExample/>
+        <Menu></Menu>
         <h2>Main Page</h2>
         </>
     )
