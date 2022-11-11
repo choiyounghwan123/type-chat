c561e1379b03d5b5beef69de0ab8df9adce736de
chigomuh <tngusrldnjs1@gmail.com>
2022-11-11 22:51:54 +0900

    Revert "Merge pull request #2 from choiyounghwan123/kw"
    This reverts commit f5485ad679d8207fe46403d1af60b23e78e84ffb, reversing
    changes made to 7d5788097ce8e9e38076871c8bc49d9d532ff1af.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./routes/Home";
-import Login from "./routes/Login";
-import Main from "./routes/Main";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './routes/Home';
+import Login from './routes/Login';
+import Main from './routes/Main';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main></Main>}></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <Route path="/main" element={<Home></Home>}></Route>
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Main></Main>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/main' element={<Home></Home>}></Route>
+      </Routes>
       </BrowserRouter>
     </div>
   );
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,44 +1,38 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { socket } from "../component/socket";
 
-const Login = (): JSX.Element => {
-  let navigate = useNavigate();
-  let [nickName, setNickName] = useState("");
+const Login = ():JSX.Element =>{
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNickName(e.target.value);
-  };
+    
 
-  const onClick = () => {
-    // 채팅방에 들어 갈 유저를 서버에 전달함
-    socket.emit("user login", nickName);
+    let navigate = useNavigate();
+    let [nickName,setNickName]=useState('');
+ 
 
-    navigate("/main", {
-      state: {
-        nickName: nickName,
-      },
-    });
-  };
 
-  const onKeyPress = (e: React.KeyboardEvent<HTMLElement>) => {
-    if (e.key === "Enter") {
-      onClick();
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        setNickName(e.target.value);
+    };
+
+    const onClick = () =>{
+        navigate('/main',{state:{
+            nickName:nickName,
+        }});
     }
-  };
 
-  return (
-    <div>
-      <p>Login Page</p>
-      <input
-        type="text"
-        placeholder="NickName"
-        onChange={onChange}
-        onKeyPress={onKeyPress}
-      />
-      <button onClick={onClick}>입장</button>
-    </div>
-  );
-};
+    const onKeyPress = (e:React.KeyboardEvent<HTMLElement>) =>{
+        if(e.key === 'Enter'){
+            onClick();
+        }
+    }
+
+    return(
+        <div>
+        <p>Login Page</p>
+        <input type='text' placeholder="NickName" onChange={onChange} onKeyPress={onKeyPress}></input>
+        <button onClick={onClick}>입장</button>
+        </div>
+    )
+}
 
 export default Login;
